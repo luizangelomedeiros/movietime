318e5e6debc7b1c82c00eda1a9c745910448e6a1
Luiz Medeiros <luizangelomedeiros@gmail.com>
2016-10-27 17:55:40 -0200

    Conteudo Projeto

---
+++ public/js/services/meus-servicos.js
@@ -0,0 +1,7 @@
+angular.module('meusServicos', ['ngResource']).factory('recursoFoto', function($resource) {
+    return $resource('/v1/fotos/:fotoId', null, {
+        'update' : { 
+            method: 'PUT'
+        }
+    });    
+});
